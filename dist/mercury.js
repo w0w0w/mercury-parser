2b563168fda3605566be87cfdf3d2d96fd6fb07c
dev <dev@abc.com>
2021-02-18 12:21:30 +0800

    build

--- dist/mercury.js
+++ dist/mercury.js
@@ -1888,13 +1888,13 @@ var TwitterExtractor = {
 var NYTimesExtractor = {
   domain: 'www.nytimes.com',
   title: {
-    selectors: ['h1.g-headline', 'h1[itemprop="headline"]', 'h1.headline']
+    selectors: ['h1.g-headline', 'h1[itemprop="headline"]', 'h1.headline', 'h1 .balancedHeadline']
   },
   author: {
-    selectors: [['meta[name="author"]', 'value'], '.g-byline', '.byline']
+    selectors: [['meta[name="author"]', 'value'], '.g-byline', '.byline', ['meta[name="byl"]', 'value']]
   },
   content: {
-    selectors: ['div.g-blocks', 'article#story'],
+    selectors: ['div.g-blocks', 'section[name="articleBody"]', 'article#story'],
     transforms: {
       'img.g-lazy': function imgGLazy($node) {
         var src = $node.attr('src');
@@ -5767,6 +5767,35 @@ var EpaperZeitDeExtractor = {
   }
 };
 
+var WwwFtchineseComExtractor = {
+  domain: 'www.ftchinese.com',
+  title: {
+    selectors: ['h1.story-headline']
+  },
+  author: {
+    selectors: ['span.story-author > a']
+  },
+  date_published: {
+    selectors: ['.story-time']
+  },
+  dek: {
+    selectors: ['.story-lead']
+  },
+  lead_image_url: {
+    selectors: [['.story-image.image >figure', 'data-url']]
+  },
+  content: {
+    selectors: ['#story-body-container'],
+    // Is there anything in the content you selected that needs transformed
+    // before it's consumable content? E.g., unusual lazy loaded images
+    transforms: {},
+    // Is there anything that is in the result that shouldn't be?
+    // The clean selectors will remove anything that matches from
+    // the result
+    clean: ['div.story-theme', 'h1.story-headline', 'div.story-byline', 'div.mpu-container-instory', 'div#story-action-placeholder', 'div.clearfloat', '.o-ads.in-article-advert']
+  }
+};
+
 
 
 var CustomExtractors = /*#__PURE__*/Object.freeze({
@@ -5903,7 +5932,8 @@ var CustomExtractors = /*#__PURE__*/Object.freeze({
   WwwPhoronixComExtractor: WwwPhoronixComExtractor,
   PitchforkComExtractor: PitchforkComExtractor,
   BiorxivOrgExtractor: BiorxivOrgExtractor,
-  EpaperZeitDeExtractor: EpaperZeitDeExtractor
+  EpaperZeitDeExtractor: EpaperZeitDeExtractor,
+  WwwFtchineseComExtractor: WwwFtchineseComExtractor
 });
 
 var Extractors = _Object$keys(CustomExtractors).reduce(function (acc, key) {
